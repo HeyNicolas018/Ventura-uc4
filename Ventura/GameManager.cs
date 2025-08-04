4bc3d81c211cd77bd32decf43965ed0ad88b7371
nicolas.fgonzales <nicolas.fgonzales@CAR0717578W11-1>
2025-08-04 17:36:23 -0300

    apenas teste de menu por enquanto

---
+++ Ventura/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventura
+{
+    public class GameManager
+    {
+
+        static private GameManager instance;
+        public static GameManager Instance = instance ??= new GameManager();
+
+
+        public void IniciaMenu()
+        {
+            MenuGame.Instance.ChamaMenu();
+        }
+    }
+}
---
+++ Ventura/VenturaProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ventura
+{
+    class VenturaProgram
+    {
+
+        public static void Main()
+        {
+           GameManager.Instance.IniciaMenu();   
+        }
+    }
+
+}
