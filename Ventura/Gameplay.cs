30f461ebed885e225c37c6027df5713b54eedcb1
andrezinhos <andre.mbmagon@CAR0717611W11-1>
2025-08-04 16:34:34 -0300

    singleton de classe do jogo gameplay

---
+++ Ventura/Gameplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventura
+{
+    public class Gameplay
+    {
+        private static Gameplay instancia;
+
+        private Gameplay() { }
+
+        public static Gameplay Instance => instancia ??= new Gameplay();
+
+
+    }
+}
