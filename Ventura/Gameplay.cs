8151aeefc7b45e89084d851a2d46d9978d686714
andrezinhos <andre.mbmagon@CAR0717611W11-1>
2025-08-05 17:20:01 -0300

    removido o for

--- Ventura/Gameplay.cs
+++ Ventura/Gameplay.cs
@@ -37,58 +37,55 @@ namespace Ventura
         {
             Console.Clear();
             
-            for (int i = 0; i < questoes.Count; i++)
-            {
-                var q = questoes[i];
-                Console.WriteLine($"""
-                #########################################
-                                Lyonei
+            Console.WriteLine($"""
+            #########################################
+                            Lyonei
 
-                                {q.r1}
+                            {q.r1}
                             
 
-                #########################################
-                """);
-                nome = Console.ReadLine();
-                Console.Clear();
-                resposta.Add(nome);
+            #########################################
+            """);
+            nome = Console.ReadLine();
+            Console.Clear();
+            resposta.Add(nome);
                 
-                Console.WriteLine($"""
-                #########################################
+            Console.WriteLine($"""
+            #########################################
                                 
 
-                              Olá {nome}
+                            Olá {nome}
 
-                        Precisamos da sua idade
-                para oferecer melhor experiencia de uso do Lyonei
+                    Precisamos da sua idade
+            para oferecer melhor experiencia de uso do Lyonei
                 
                             
 
-                #########################################
-                """);
-                idade = Console.ReadLine();
-                Console.Clear();
-                resposta.Add(idade);
+            #########################################
+            """);
+            idade = Console.ReadLine();
+            Console.Clear();
+            resposta.Add(idade);
                 
-                Console.WriteLine($"""
-                #########################################
+            Console.WriteLine($"""
+            #########################################
                                 
 
-                              Olá {nome}
+                            Olá {nome}
 
-                    Precisamos coletar seu ano de nascimento
-                por conta dos nossos termos de uso do Lyonei
+                Precisamos coletar seu ano de nascimento
+            por conta dos nossos termos de uso do Lyonei
                 
-                               {q.r1}
-                               {q.r2}
-                               {q.r3}
+                            {q.r1}
+                            {q.r2}
+                            {q.r3}
 
-                #########################################
-                """);
-                ano = Console.ReadLine();
-                resposta.Add(ano);
-                Console.Clear();
-            }
+            #########################################
+            """);
+            ano = Console.ReadLine();
+            resposta.Add(ano);
+            Console.Clear();
+         
         }
  
     }
